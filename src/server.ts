01fe77797db21a595883f478d9f631e7f96dddb7
kei <tenguuenglish@gmail.com>
2020-04-26 17:46:55 +0900

    :sparkles: Completed

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import "dotenv/config";
+import * as grpc from "grpc";
+
+import { protoIndex } from "./proto";
+import greeterHandler from "./handlers/greeter";
+
+protoIndex();
+
+const port: string | number = process.env.PORT || 50051;
+
+type StartServerType = () => void;
+export const startServer: StartServerType = (): void => {
+  const server: grpc.Server = new grpc.Server();
+
+  server.addService(greeterHandler.service, greeterHandler.handler);
+
+  server.bindAsync(
+    `0.0.0.0:${port}`,
+    grpc.ServerCredentials.createInsecure(),
+    (err: Error, port: number) => {
+      if (err !== null) {
+        return console.error(err);
+      }
+      console.log(`gRPC listening on ${port}`);
+    }
+  );
+
+  server.start();
+};
+
+startServer();
